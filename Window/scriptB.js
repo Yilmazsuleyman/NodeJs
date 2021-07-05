fb86250190f74c8170fa1bc6d52ffa77383d22db
SüleymanYılmaz <suleymanyilmaz95@gmail.com>
2021-07-05 12:53:29 +0300

    Window ve Global yapılar

--- Window/scriptB.js
+++ Window/scriptB.js
@@ -2,6 +2,19 @@ var lastName = "Yılmaz";
 
 console.log(lastName);
 console.log(firstName);
-
+console.log(window);
 
 //Tarayıcıya aktarım yapıyor fakat node içinde görmüyor.
+
+
+/*
+                    //////////     window     \\\\\\\\\\\
+                        |                           |
+                        |                           |    
+                        |                           |
+                        |                           |
+                        v                           v
+                     scriptA                     scriptB
+
+        Scriptler window üzerinden istedikleri bilgileri beraber kullanabilir. Sadece tarayıcı üzerinde göstermek için kullanılır. Global bir değişken değildir. Global değişkenler için module ler kullanılır.
+*/ 
