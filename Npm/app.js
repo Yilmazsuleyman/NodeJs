9ab8a77253727c35c564da9b39b246a32c0c0c20
SüleymanYılmaz <suleymanyilmaz95@gmail.com>
2021-07-06 14:18:54 +0300

    Npm kurulum ve temeller

---
+++ Npm/app.js
@@ -0,0 +1,6 @@
+// Terminale npm init diyerek package.json oluşturabiliyoruz. npm init --yes direkt oluşturur.
+
+var _ = require('underscore');
+
+var numbers = [10,5,200,350,1,321,421];
+console.log(_.min(numbers));
