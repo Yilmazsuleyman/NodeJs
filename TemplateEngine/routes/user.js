0bdd262b7198bee02b232096b5502e8dead85cdf
SüleymanYılmaz <suleymanyilmaz95@gmail.com>
2021-07-07 20:29:57 +0300

    Template Engine Temelleri

--- TemplateEngine/routes/user.js
+++ TemplateEngine/routes/user.js
@@ -6,7 +6,7 @@ const admin = require('./admin');
 
 router.get('/', (req, res, next) => {
 
-    res.render('index', {title: "Homepage", products: admin.products});
+    res.render('index', {title: "Homepage", products: admin.products, path: '/'});
 });
 router.use((req, res)=>{
     res.status(404).render('404', {title: "404 Page"});
