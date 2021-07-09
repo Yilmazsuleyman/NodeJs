0cc3634d211c03a10d4eb25917e59b23291bf1e1
SüleymanYılmaz <suleymanyilmaz95@gmail.com>
2021-07-09 14:19:42 +0300

    Controller

---
+++ Mvc/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const productsController = require('../controllers/product');
+const errorsController = require('../controllers/errors');
+
+
+router.get('/', productsController.getProducts);
+
+router.use(errorsController.get404Page);
+
+module.exports = router;
